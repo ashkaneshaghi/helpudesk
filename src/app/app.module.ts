873ca5e366be06b35eae45542469d0bb463b897a
Ashkan Es haghi <45358130+ashkaneshaghi@users.noreply.github.com>
2019-05-02 15:37:43 +0200

    05-02-2019 15:37

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { IonicSelectableModule } from 'ionic-selectable'
+import { IonicSelectableModule } from 'ionic-selectable'; 
 
 @NgModule({
   declarations: [AppComponent],
